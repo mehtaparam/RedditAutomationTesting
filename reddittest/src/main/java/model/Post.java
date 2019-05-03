364f63a668afa3e0e7641ff3bb0411dcf6b0bb91
Param Mehta <mehtaparam@ymail.com>
2019-05-03 21:04:17 +0530

    Added Post Model

---
+++ reddittest/src/main/java/model/Post.java
@@ -0,0 +1,62 @@
+package model;
+
+public class Post {
+	String catagory;
+	String title;
+	String text;
+	String mediaURL;
+	String upVotes;
+	String userName;
+	String subReddit;
+	String mediaType;
+	public String getCatagory() {
+		return catagory;
+	}
+	public void setCatagory(String catagory) {
+		this.catagory = catagory;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getMediaURL() {
+		return mediaURL;
+	}
+	public void setMediaURL(String mediaURL) {
+		this.mediaURL = mediaURL;
+	}
+	public String getUpVotes() {
+		return upVotes;
+	}
+	public void setUpVotes(String upVotes) {
+		this.upVotes = upVotes;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getSubReddit() {
+		return subReddit;
+	}
+	public void setSubReddit(String subReddit) {
+		this.subReddit = subReddit;
+	}
+	public String getMediaType() {
+		return mediaType;
+	}
+	public void setMediaType(String mediaType) {
+		this.mediaType = mediaType;
+	}
+	
+	
+}
